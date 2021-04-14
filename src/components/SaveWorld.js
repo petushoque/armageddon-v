53cfb2aa2eb87fb7ac219a730789476b6736f9fe
Alexei Kubasov <kubasovalexei@yandex.ru>
2021-04-14 17:48:33 +0700

    feat: добавить отдельный блок с отправкой команды по уничтожению выбранных объектов

--- src/components/SaveWorld.js
+++ src/components/SaveWorld.js
@@ -5,16 +5,14 @@ function SaveWorld (props) {
 
     function test () {console.log(props.destroyList)
 
-        let oldElements = document.querySelectorAll('.alert'); // обращаю внимание на точку
-
-        // Вариант адын
+        let oldElements = document.querySelectorAll('.save-world__item');
         for( let i = 0; i < oldElements.length; i++ ){
-            oldElements[i].outerHTML = "";
+            oldElements[i].outerHTML = '';
         }
     
         props.destroyList.forEach((element) => {
             let asteroid = document.createElement('p');
-            asteroid.className = "alert";
+            asteroid.className = 'save-world__item';
             asteroid.innerHTML = element;
             document.querySelector('.save-world__list').append(asteroid);
         })
@@ -22,19 +20,30 @@ function SaveWorld (props) {
         
     }
     
-    function testTest () {props.resetList()
+    function destroyAsteroids () {
+        props.resetList()
 
+        let oldElements = document.querySelectorAll('.save-world__item');
+        for( let i = 0; i < oldElements.length; i++ ){
+            oldElements[i].outerHTML = '';
+        }
     }
 
     return (
         <div className='save-world'>
             <h2 className='save-world__title'>Список астероидов на уничтожение:</h2>
-            <button onClick={test}>SHOW LOST</button>
-            <button onClick={testTest}>RESET LOST</button>
+            <button className='save-world__show-list-button' onClick={test}>
+            &#128209; Загрузить список
+            </button>
+            
             <div className='save-world__list'>
-
             </div>
             
+            <button className='save-world__destroy-button' onClick={destroyAsteroids}>
+            &#128640; Поехали!
+            </button>
+
+            <p className='save-world__description'>*Даю согласие на отправку бригады имени Брюса Уиллиса на выбранные астероиды &#128221;</p>
             
         </div>
     )
